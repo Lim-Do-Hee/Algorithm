a8eef05013afc9ce65c2dc8afbc15e530c57e0ec
DoHee <dohee3959@gmail.com>
2022-07-27 20:51:04 +0900

    [Do it]최댓값, 중앙값 연습문제

--- Do it!/src/기본알고리즘/Max3.java
+++ Do it!/src/기본알고리즘/Max3.java
@@ -35,3 +35,42 @@ class Max3m {
         System.out.println("max(2, 3, 1) = " + max3(2, 3, 1));
     }
 }
+
+/** Q1 네 값의 최댓값 */
+class Max4 {
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (d > max) max = d;
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("max(1, 2, 3, 4) = " + max4(1, 2, 3, 4));
+    }
+}
+
+/** Q2 세 값의 최솟값 */
+class Min3 {
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        return min;
+    }
+}
+
+/** Q3 네 값의 최솟값 */
+class Min4 {
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+
+        return min;
+    }
+}
--- Do it!/src/기본알고리즘/Median.java
+++ Do it!/src/기본알고리즘/Median.java
@@ -34,3 +34,45 @@ public class Median {
 
     }
 }
+
+/** Q4 세 값의 대소관계 13종류 중앙값 */
+class Median13 {
+    static int median13(int a, int b, int c) {
+        if (a >= b)
+            if (b >= c)
+                return b;
+            else if (c >= a)
+                return a;
+            else
+                return c;
+        else if (a > c)
+            return a;
+        else if (b > c)
+            return c;
+        else
+            return b;
+    }
+}
+
+/** Q5 med3에 비해 효율이 떨어지는 이유 */
+/** or로 되어 있어 조건을 다 봐야 한다.
+ *   처음 if 문의 판단
+ * 　if ((b >= a && c<= a) || (b <= a && c >= a)
+ *  에 주목합니다.
+ *   여기서 b >= a 및 b <= a의 판단을 뒤집은 판단
+ *   (실질적으로 동일한 판단)이, 계속하여  else 이후에서
+ * 　　 else if ((a > b && c < b) || (b <= a && c > b)
+ *   로 수행됩니다. 즉, 처음 if가 성립하지 않는 경우,
+ *   2번째 if에서도 (실질적으로 ) 같은 판단을 수행하므로,
+ *   효율이 나빠집니다.
+ * */
+
+class badMed3 {
+    static int badMed3(int a, int b, int c) {
+        if ((b >= a && c <= a) || ((b <= a && c >= a)))
+            return a;
+        else if ((a > b && c < b) || (a < b && c > b))
+            return b;
+        return c;
+    }
+}
