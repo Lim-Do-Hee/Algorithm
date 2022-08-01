097db87fda4dd000329c42560d70fba21ee64dad
DoHee <dohee3959@gmail.com>
2022-08-01 22:13:28 +0900

    [Do it] 양수만 더하기

--- Do it!/src/기본알고리즘/SumFor.java
+++ Do it!/src/기본알고리즘/SumFor.java
@@ -20,3 +20,78 @@ public class SumFor {
 
     }
 }
+
+/** Q7 1 ~ 7 합 */
+class SumFor7 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 7; i++) {
+            sum += i;
+        }
+
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 = " + sum);
+    }
+}
+
+/** Q8 가우스의 덧셈 */
+class Gauss {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int n = stdIn.nextInt();
+
+        int halfN = n / 2;
+        int sum = 0;
+
+        // n이 짝수
+        if (n % 2 == 0)
+            sum = (1 + n) * halfN;
+        else
+            sum = (1 + n) * (halfN) + (halfN + 1);
+
+        int sum2 = (n + 1) * (n / 2) + (n % 2 == 1 ? (n + 1) / 2 : 0);
+
+        System.out.println(sum);
+    }
+}
+
+/** Q9 a, b를 포함하여 그 사의 모든 정수의 합 */
+class SumOf {
+    static int sumOf(int a, int b) {
+
+        int sum = 0;
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (; a <= b; a++) {
+            sum += a;
+        }
+
+        return sum;
+    }
+
+    static int sumOf2(int a, int b) {
+        int min;		// a, b의 작은 쪽의 값
+        int max;		// a, b의 큰 쪽의 값
+
+        if (a < b) {
+            min = a;  max = b;
+        } else {
+            min = b;  max = a;
+        }
+
+        int sum = 0;		// 총합
+        for (int i = min; i <= max; i++)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOf(3, 5));
+        System.out.println(sumOf(6, 4));
+    }
+}
