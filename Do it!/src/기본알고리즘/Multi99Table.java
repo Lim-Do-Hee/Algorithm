a47add78792df9d43e52d8b09ab660beb2d38d58
DoHee <dohee3959@gmail.com>
2022-08-05 20:26:38 +0900

    [Do it]곱셈표

--- Do it!/src/기본알고리즘/Multi99Table.java
+++ Do it!/src/기본알고리즘/Multi99Table.java
@@ -21,6 +21,12 @@ public class Multi99Table {
 class Multiple99Table {
     public static void main(String[] args) {
 
+        System.out.printf("%3s", "|");
+        for (int i = 1; i <= 9; i++)
+            System.out.printf("%3d", i);
+//        System.out.println();
+        System.out.println("\n---+---------------------------");
+
         for (int i = 1; i <= 9; i++) {
             System.out.print(i + " |");
             for (int j = 1; j <= 9; j++) {
