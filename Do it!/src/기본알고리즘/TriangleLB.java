d66f467695097e2c622db2792940b9863948b1f8
DoHee <dohee3959@gmail.com>
2022-08-08 21:38:49 +0900

    [Do it] Q15, Q16, Q17

--- Do it!/src/기본알고리즘/TriangleLB.java
+++ Do it!/src/기본알고리즘/TriangleLB.java
@@ -30,7 +30,7 @@ class Triangle {
     static void triangleLB(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++)
-                System.out.print("*");
+                System.out.print('*');
             System.out.println();
         }
     }
@@ -44,23 +44,120 @@ class Triangle {
         }
     }
 
+    static void triangleLU2(int n) {
+        for (int i = 1; i <= n; i++) {            // i 행( i = 1, 2, ... , n )
+            for (int j = 1; j <= n - i + 1; j++)         // n - i + 1 개의 '*'을  출력
+                System.out.print('*');
+            System.out.println();                  // 줄바꿈
+        }
+    }
+
     /** 오른쪽 위 */
     static void triangleRU(int n) {
 
-//        for (int i = n; i >= 1; i--)
-//            System.out.print(" " * (n - i) + '*');
-//        for (int i = n; i >= 1; i--) {
-//            for (int j = 1; j <= i; j++)
-//                System.out.print();
-//            System.out.println();
-//        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++)
+                System.out.print(j > n - i ? '*' : ' ');
+            System.out.println();
+        }
     }
 
+    static void triangleRU2(int n) {
+        for (int i = 1; i <= n; i++) {            // i 행( i = 1, 2, ... , n )
+            for (int j = 1; j <= i - 1; j++)         // i - 1 개의 ' '을  출력
+                System.out.print(' ');
+            for (int j = 1; j <= n - i + 1; j++)   // n - i + 1 개의 '*'을  출력
+                System.out.print('*');
+            System.out.println();                  // 줄바꿈
+        }
+    }
 
+    /** 오른쪽 아래 */
+    static void triangleRB(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++)
+                System.out.print(j > n - i ? '*' : ' ');
+            System.out.println();
+        }
+    }
+
+    static void triangleRB2(int n) {
+        for (int i = 1; i <= n; i++) {            // i 행( i = 1, 2, ... , n )
+            for (int j = 1; j <= n - i; j++)         // n - i 개의 ' '을  출력
+                System.out.print(' ');
+            for (int j = 1; j <= i; j++)            // i 개의 '*'을  출력
+                System.out.print('*');
+            System.out.println();                  // 줄바꿈
+        }
+    }
 
     public static void main(String[] args) {
         triangleLB(5);
+        System.out.println();
         triangleLU(5);
+        System.out.println();
         triangleRU(5);
+        System.out.println();
+        triangleRB(5);
+    }
+}
+
+/** Q16 n단의 피라미드 */
+class Spira {
+    static void spira(int n) {
+
+        int range = (n - 1) * 2 + 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= range; j++)
+                System.out.print(i > Math.abs(n - j) ? '*' : ' ');
+            System.out.println();
+        }
+    }
+
+    static void spira2(int n) {
+        for (int i = 1; i <= n; i++) {					// i 행( i = 1, 2, ... , n )
+            for (int j = 1; j <= n - i; j++)			// n - i 개의 ' ' 을 출력
+                System.out.print(' ');
+            for (int j = 1; j <= (i-1)*2+1; j++)	// (i - 1) * 2 + 1 개의 '*' 을 출력
+                System.out.print('*');
+            System.out.println();									// 줄바꿈
+        }
+    }
+
+    public static void main(String[] args) {
+        spira(4);
+    }
+}
+
+/** Q17 n단의 숫자 피라미드 */
+class Npira {
+    static void npira(int n) {
+
+        int range = (n - 1) * 2 + 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= range; j++)
+                if (i > Math.abs(n - j))
+                    System.out.print(i);
+                else
+                    System.out.print(' ');
+            System.out.println();
+        }
+    }
+
+    static void npira2(int n) {
+        for (int i = 1; i <= n; i++) {					// i 행( i = 1, 2, ... , n )
+            for (int j = 1; j <= n - i; j++)			// n - i 개의 ' ' 을 출력
+                System.out.print(' ');
+            for (int j = 1; j <= (i-1)*2+1; j++)	// (i - 1) * 2 + 1개의 '*' 을 출력
+                System.out.print(i % 10);
+            System.out.println();									// 줄바꿈
+        }
+    }
+
+    public static void main(String[] args) {
+        npira(4);
     }
 }
