2df0e9a638ebf8c095805bfe4b0ea04c670de875
DoHee <dohee3959@gmail.com>
2022-08-10 21:25:09 +0900

    [Do it] 기본자료구조 배열

---
+++ Do it!/src/기본자료구조/배열/IntArrayInit.java
@@ -0,0 +1,12 @@
+package 기본자료구조.배열;
+
+/** 배열 초기화 */
+public class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+//        int[] a = new int[] {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < a.length; i++)
+            System.out.println("a[" + i + "] = " + a[i]);
+    }
+}
