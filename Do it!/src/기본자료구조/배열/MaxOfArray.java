a3ec3ba173cd7e8744d1eab99901af4c56539fda
DoHee <dohee3959@gmail.com>
2022-08-11 21:10:14 +0900

    [Do it] 배열 최댓값 요소

---
+++ Do it!/src/기본자료구조/배열/MaxOfArray.java
@@ -0,0 +1,30 @@
+package 기본자료구조.배열;
+
+import java.util.Scanner;
+
+public class MaxOfArray {
+    static int maxOf(int[] a) {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++)
+            if (a[i] > max)
+                max = a[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("키의 최댓값을 구합니다.");
+        System.out.print("사람 수 : ");
+        int num = stdIn.nextInt();
+
+        int[] height = new int[num]; // 요솟수가 num인 배열을 생성
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("height[" + i + "] : ");
+            height[i] = stdIn.nextInt();
+        }
+
+        System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+    }
+}
