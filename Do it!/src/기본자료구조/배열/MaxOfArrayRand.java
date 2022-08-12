df9708a6fdaa44650d60f2c65d7e0b0bbf3d48b2
DoHee <dohee3959@gmail.com>
2022-08-12 19:48:27 +0900

    [Do it] Q1.난수 최대요솟값

--- Do it!/src/기본자료구조/배열/MaxOfArrayRand.java
+++ Do it!/src/기본자료구조/배열/MaxOfArrayRand.java
@@ -3,6 +3,7 @@ package 기본자료구조.배열;
 import java.util.Random;
 import java.util.Scanner;
 
+/** 난수 사용 배열 요솟값 */
 public class MaxOfArrayRand {
     static int maxOf(int[] a) {
         int max = a[0];
@@ -30,3 +31,33 @@ public class MaxOfArrayRand {
         System.out.println("최댓값은 " + maxOf(height) + "입니다.");
     }
 }
+
+/** Q1 사람수도 난수로 */
+class MaxOfArrayRand2 {
+    static int maxOf(int[] a) {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++)
+            if (a[i] > max)
+                max = a[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        System.out.println("키의 최댓값을 구합니다.");
+        System.out.print("사람 수 : ");
+        int num = rand.nextInt(100);
+
+        int[] height = new int[num]; // 요솟수가 num인 배열을 생성
+
+        for (int i = 0; i < num; i++) {
+            height[i] = 100 + rand.nextInt(90); // 요소의 값을 난수로 결정
+            System.out.println("height[" + i + "] : " + height[i]);
+        }
+
+        System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+    }
+}
+
+
