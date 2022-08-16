5d9b1f1690a0bb66699b4c55c278aa8274a30fbc
DoHee <dohee3959@gmail.com>
2022-08-16 21:37:23 +0900

    [Do it] 배열 Q4, Q5

--- Do it!/src/기본자료구조/배열/ArrayEqual.java
+++ Do it!/src/기본자료구조/배열/ArrayEqual.java
@@ -1,5 +1,6 @@
 package 기본자료구조.배열;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /** 두 배열의 비교 */
@@ -41,5 +42,57 @@ class ArrayEqual {
 
         System.out.println("배열 a와 b는 " + (equals(a, b) ? "같습니다." : "같지 않습니다."));
 
+    }
+}
+
+/** Q4 copy */
+class Copy {
+    static void copy(int[] a, int[] b) {
+        int num = a.length <= b.length ? a.length : b.length;
+        for (int i = 0; i < num; i++)
+            a[i] = b[i];
+    }
+
+    public static void main(String[] args) {
+        int[] x = {1, 2, 3};
+        int[] y = {4, 5};
+        copy(x, y);
+        System.out.println(Arrays.toString(x));
+    }
+}
+
+/** Q5 역순으로 복사 */
+class Rcopy {
+
+    static void rcopy(int[] a, int[] b) {
+        int num = a.length <= b.length ? a.length : b.length;
+        for (int i = 0; i < num; i++)
+            a[i] = b[b.length - i - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("a의 요솟수는 : ");
+        int numa = stdIn.nextInt();		// 요솟수
+        int[] a = new int[numa];			// 요솟수가 numa인 배열
+        for (int i = 0; i < numa; i++) {
+            System.out.print("a[" + i + "] : ");
+            a[i] = stdIn.nextInt();
+        }
+
+        System.out.print("b의 요솟수는  : ");
+        int numb = stdIn.nextInt();		// 요솟수
+        int[] b = new int[numb]; 			// 요솟수가 numb인 배열
+        for (int i = 0; i < numb; i++) {
+            System.out.print("b[" + i + "] : ");
+            b[i] = stdIn.nextInt();
+        }
+
+        rcopy(a, b);			// 배열 b의 모든 요소를 배열 a에 역순으로 copy
+
+        System.out.println("배열 b의 모든 요소를 배열 a에 역순으로 copy 하였습니다.");
+        for (int i = 0; i < numa; i++)
+            System.out.println("a[" + i + "] = " + a[i]);
     }
 }
