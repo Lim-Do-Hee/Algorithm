3ef052522ab198af7826a024abf6cd72c82fa792
DoHee <dohee3959@gmail.com>
2022-08-26 21:00:48 +0900

    [Do it] Q9 남은 일수

--- Do it!/src/기본자료구조/배열/DayOfYear.java
+++ Do it!/src/기본자료구조/배열/DayOfYear.java
@@ -84,3 +84,43 @@ class DayOfYearWhile {
         } while (retry == 1);
     }
 }
+
+/** Q9 y년 m월 d일의 그 해 남은 일 수 */
+class LeftDayOfYear {
+    // 각 달의 일수
+    static int[][] mdays = {
+            {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31}, // 평년
+            {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31}  // 윤년
+    };
+
+    // 서기 year년은 윤년인가? (윤년: 1 / 평년: 0)
+    static int isLeap(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 1: 0;
+    }
+
+    static int leftDayOfYear(int y, int m, int d) {
+        int days = mdays[isLeap(y)][m - 1] - d; // m월에 남은 일수
+
+        for (; m < 12; m++) // m + 1 월부터 일수 더하기
+            days += mdays[isLeap(y)][m];
+        return days;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int retry; // 다시 한번?
+
+        System.out.println("그 해 남은 일수를 구합니다.");
+
+        do {
+            System.out.print("년 : "); int year = stdIn.nextInt(); // 년
+            System.out.print("월 : "); int month = stdIn.nextInt(); // 월
+            System.out.print("일 : "); int day = stdIn.nextInt(); // 일
+
+            System.out.printf("그 해 %d일 남았습니다.\n", leftDayOfYear(year, month, day));
+
+            System.out.print("한 번 더 할까요? (1.예 / 0.아니오) : ");
+            retry = stdIn.nextInt();
+        } while (retry == 1);
+    }
+}
