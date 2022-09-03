7dc39af5c61b343162945f3ed990027597c1b9c3
DoHee <dohee3959@gmail.com>
2022-09-03 22:03:02 +0900

    [Do it] Q1.선형검색 for문

--- Do it!/src/CH03/검색/선형검색/SeqSearchSen.java
+++ Do it!/src/CH03/검색/선형검색/SeqSearchSen.java
@@ -42,3 +42,42 @@ class SeqSearchSen {
             System.out.println(ky + "은(는) x[" + idx + "]에 있습니다");
     }
 }
+
+/** Q1 seqSearchSen for문*/
+class SeqSearchSenFor {
+
+    // 요솟수가 n인 배열 a에서 key와 같은 요소를 보초법으로 선형 검색합니다.
+    static int seqSearchSenFor(int[] a, int n, int key) {
+
+        a[n] = key; // 보초를 추가
+
+        for (int i = 0; i < n; i++)
+            if (a[i] == key)
+                return i;
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num + 1]; // 요솟수가 num인 배열
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.print("검색할 값 : "); // 키 값을 입력
+        int ky = stdIn.nextInt();
+
+        int idx = seqSearchSenFor(x, num, ky); // 배열 x에서 키 값이 ky인 요소를 검색
+
+        if (idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다");
+    }
+}
