90ddb40d9270e9592cce6fe1f497413b1f129003
DoHee <dohee3959@gmail.com>
2022-09-07 19:19:52 +0900

    [Do it] CH03 Q5. 맨앞의 요소

--- Do it!/src/CH03/검색/이진검색/BinSearch.java
+++ Do it!/src/CH03/검색/이진검색/BinSearch.java
@@ -85,6 +85,27 @@ class searchIdx{
         return count;
     }
 
+    //--- 배열 a의 앞쪽 n개의 요소에서 key와 일치하는 요소를 이진검색 ---//
+    static int binSearchX2(int[] a, int n, int key) {
+        int pl = 0;			// 검색 범위 맨앞의 인덱스
+        int pr = n - 1;		// 　 〃    　맨끝의 인덱스
+
+        do {
+            int   pc = (pl + pr) / 2;	// 중앙요소의 인덱스
+            if (a[pc] == key) {
+                for ( ; pc > pl; pc--)	// key와 같은 맨앞의 요소를 검색합니다
+                    if (a[pc - 1] < key)
+                        break;
+                return pc;							// 검색 성공
+            } else if (a[pc] < key)
+                pl = pc + 1;						// 검색 범위를 앞쪽 절반으로 좁힘
+            else
+                pr = pc - 1;						// 검색 범위를 뒤쪽 절반으로 좁힘
+        } while (pl <= pr);
+
+        return -1;									// 검색 실패
+    }
+
     public static void main(String[] args) {
         System.out.println(seqSearchIdx(new int[] {1, 9, 2, 9, 4, 6, 7, 9}, 8, 9, new int[3]));
         System.out.println(seqSearchIdx(new int[] {1, 9, 2, 9, 4, 6, 7, 9}, 8, 9, new int[3]));
