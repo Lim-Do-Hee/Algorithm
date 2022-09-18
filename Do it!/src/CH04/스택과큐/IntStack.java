f12c2f77eea89232c5bbea127b0a249a332baa7e
DoHee <dohee3959@gmail.com>
2022-09-18 20:31:40 +0900

    [Do it] CH04. int형 스택(푸쉬, 팝)

--- Do it!/src/CH04/스택과큐/IntStack.java
+++ Do it!/src/CH04/스택과큐/IntStack.java
@@ -26,4 +26,18 @@ public class IntStack {
             max = 0;
         }
     }
+
+    // 스택에 x를 푸시
+    public int push(int x) throws OverflowIntStackException {
+        if (ptr >= max) // 스택이 가득 참
+            throw new OverflowIntStackException();
+        return stk[ptr++] = x; // x를 저장한 후의 stk[ptr]값을 반환
+    }
+
+    // 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
+    public int pop() throws EmptyIntStackException {
+        if (ptr <= 0) // 스택이 비어있음
+            throw new EmptyIntStackException();
+        return stk[--ptr];
+    }
 }
