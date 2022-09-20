94b6d5823e562fbfc8a14738393a5a204e47633d
DoHee <dohee3959@gmail.com>
2022-09-20 20:01:52 +0900

    [Do it] int향 스택 메소드 추가

--- Do it!/src/CH04/스택과큐/IntStack.java
+++ Do it!/src/CH04/스택과큐/IntStack.java
@@ -36,8 +36,61 @@ public class IntStack {
 
     // 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄)
     public int pop() throws EmptyIntStackException {
-        if (ptr <= 0) // 스택이 비어있음
-            throw new EmptyIntStackException();
+        if (ptr <= 0) // 스택이 비어있음음
+           throw new EmptyIntStackException();
         return stk[--ptr];
     }
+
+    // 스택에서 데이터를 피크(정사에 있는 데이터를 들여다봄)
+    public int peek() throws EmptyIntStackException {
+        if (ptr <= 0)
+            throw new EmptyIntStackException();
+        return stk[ptr - 1];
+    }
+
+    // 스택에서 x를 찾아 인덱스(없으면 -1)를 반환
+    public int indexOf(int x) {
+        for (int i = ptr - 1; i >= 0; i--) // 정상 쪽에서 선형 검색
+            if (stk[i] == x)
+                return i; // 검색 성공
+        return -1; // 검색 실패
+    }
+
+    // 스택을 비움 - 스택에 대한 푸시와 팝 등 모든 작업은 스택 포인터를 바탕으로 이루어집니다.
+    // 따라서 스택의 배열 요솟값을 변경할 필요가 없습니다.
+    // 모든 요소의 삭제는 스택 포인터 ptr 값을 0으로 하면 끝납니다.
+    public void clear() {
+        ptr = 0;
+    }
+
+    // 스택의 용량을 반환
+    public int capacity() {
+        return max;
+    }
+
+    // 스택에 쌓여있는 데이터 수를 반환
+    public int size() {
+        return ptr;
+    }
+
+    // 스택이 비어 있는가?
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+
+    // 스택이 가득 찼는가?
+    public boolean isFull() {
+        return ptr >= max;
+    }
+
+    // 스택 안의 모든 데이터를 바닥 -> 꼭대기 순서로 출력
+    public void dump() {
+        if (ptr <= 0)
+            System.out.println("스택이 비어 있습니다.");
+        else
+            for (int i = 0; i < ptr; i++)
+                System.out.println(stk[i] + " ");
+            System.out.println();
+    }
+
 }
