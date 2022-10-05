7b9e15b40740c2aad967bcc3afe66a54082e5622
DoHee <dohee3959@gmail.com>
2022-10-05 19:08:58 +0900

    [Do it] CH05.Q1 재귀없이 factorial

--- Do it!/src/CH05/재귀알고리즘/재귀의기본/Factorial.java
+++ Do it!/src/CH05/재귀알고리즘/재귀의기본/Factorial.java
@@ -20,3 +20,23 @@ public class Factorial {
         System.out.println(x + "의 팩토리얼은 " + factorial(x) + "입니다.");
     }
 }
+
+/** Q1. 재귀 없이 factorial */
+class FactorialNoRecur {
+    static int factorialNoRecur(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요. : ");
+        int x = stdIn.nextInt();
+
+        System.out.println(x + "의 팩토리얼은 " + factorialNoRecur(x) + "입니다.");
+    }
+}
