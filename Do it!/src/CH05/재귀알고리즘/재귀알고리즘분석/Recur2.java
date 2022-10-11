8d4a7ad35fd2cefe91eb5c03bf4476b0b5a431c2
DoHee <dohee3959@gmail.com>
2022-10-11 20:24:50 +0900

    [Do it] CH05. Q4 하향식, 상향식 분석

--- Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur2.java
+++ Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur2.java
@@ -2,16 +2,26 @@ package CH05.재귀알고리즘.재귀알고리즘분석;
 
 import java.util.Scanner;
 
-/** Q2. 하향식, 상향식 분석 수행 */
-class Recur2 {
+/** 재귀함수 이해하기 */
+class Recur {
     // 재귀 함수
-    static void recur2(int n) {
+    static void recur(int n) {
         if (n > 0) {
-            recur2(n - 2);
+            recur(n - 1);
             System.out.println(n);
-            recur2(n - 1);
+            recur(n - 2);
         }
     }
+
+    // 꼬리 재귀를 제거
+    static void recur2(int n) {
+        while (n > 0) {
+            recur2(n - 1);
+            System.out.println(n);
+            n = n - 2;
+        }
+    }
+
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
@@ -19,6 +29,6 @@ class Recur2 {
         System.out.print("정수를 입력하세요. : ");
         int x = stdIn.nextInt();
 
-        recur2(x);
+        recur(x);
     }
 }
