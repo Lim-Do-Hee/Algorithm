468aeb841c65627c2d9d45ce0b1f11bcf2d59e66
DoHee <dohee3959@gmail.com>
2022-10-15 20:14:19 +0900

    [Do it] CH05. Q5 Recur3 구현중

--- Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
+++ Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
@@ -3,21 +3,31 @@ package CH05.재귀알고리즘.재귀알고리즘분석;
 /** */
 public class Recur3 {
     static void recur3(int n) {
-        IntStack s = new IntStack(n);
+        IntStack nStk = new IntStack(n);
+//        IntStack sStk = new IntStack(n);
+        int sw = 0;
 
         while (true) {
             if (n > 0) {
-                s.push(n);
-                n -= 1;
-                continue;
-            }
-            if (!s.isEmpty()) { // 스택이 비어있지 않다면
-                int m;
-                m = n = s.pop(); // 저장하고 있떤 스택의 값을 팝
-                System.out.println(n);
+                nStk.push(n);
+                if (sw == 0)
+                    n -= 1;
+                else if (sw == 1) {
+                    n -= 2;
+                    sw = 0;
+                }
                 continue;
             }
 
+            if (!nStk.isEmpty()) { // 스택이 비어있지 않다면
+                sw += 1;
+                n = nStk.pop(); // 저장하고 있떤 스택의 값을 팝
+                if (sw == 2)
+                    System.out.println();
+                else
+                    nStk.push(n);
+                continue;
+            }
             break;
         }
     }
