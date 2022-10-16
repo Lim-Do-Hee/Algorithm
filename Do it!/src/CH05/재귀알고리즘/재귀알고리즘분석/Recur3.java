5ae8ea1cf1598a138f3b9682d39b754300333cc0
DoHee <dohee3959@gmail.com>
2022-10-16 23:15:53 +0900

    [Do it] CH05. Q5 Recur3 스택 이용

--- Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
+++ Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
@@ -3,13 +3,14 @@ package CH05.재귀알고리즘.재귀알고리즘분석;
 /** */
 public class Recur3 {
     static void recur3(int n) {
-        IntStack nStk = new IntStack(n);
-//        IntStack sStk = new IntStack(n);
+        IntStack nStk = new IntStack(n * 10);
+        IntStack sStk = new IntStack(n * 10);
         int sw = 0;
 
         while (true) {
             if (n > 0) {
                 nStk.push(n);
+                sStk.push(sw);
                 if (sw == 0)
                     n -= 1;
                 else if (sw == 1) {
@@ -20,12 +21,13 @@ public class Recur3 {
             }
 
             if (!nStk.isEmpty()) { // 스택이 비어있지 않다면
-                sw += 1;
                 n = nStk.pop(); // 저장하고 있떤 스택의 값을 팝
-                if (sw == 2)
-                    System.out.println();
-                else
-                    nStk.push(n);
+                sw = sStk.pop() + 1;
+                if (sw == 2) {
+                    System.out.println(n);
+                    n = nStk.pop();
+                    sw = sStk.pop() + 1;
+                }
                 continue;
             }
             break;
@@ -33,6 +35,6 @@ public class Recur3 {
     }
 
     public static void main(String[] args) {
-        recur3(3);
+        recur3(4);
     }
 }
