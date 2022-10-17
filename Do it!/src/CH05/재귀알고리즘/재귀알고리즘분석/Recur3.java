9e78eac1f7093eb5dbe53e8b7ba37d66f91afcdd
DoHee <dohee3959@gmail.com>
2022-10-17 22:00:10 +0900

    [Do it] CH05. Q5 Recur3

--- Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
+++ Do it!/src/CH05/재귀알고리즘/재귀알고리즘분석/Recur3.java
@@ -1,6 +1,8 @@
 package CH05.재귀알고리즘.재귀알고리즘분석;
 
-/** */
+import java.util.Scanner;
+
+/** Q5 Recur3 재귀제거 */
 public class Recur3 {
     static void recur3(int n) {
         IntStack nStk = new IntStack(n * 10);
@@ -38,3 +40,50 @@ public class Recur3 {
         recur3(4);
     }
 }
+
+class Recur3A {
+
+    //--- 메서드 recur의 비재귀적 구현 ---//
+    static void recur3(int n) {
+        int[] nstk = new int[100];
+        int[] sstk = new int[100];
+        int ptr = -1;
+        int sw = 0;
+
+        while (true) {
+            if (n > 0) {
+                ptr++;
+                nstk[ptr] = n;
+                sstk[ptr] = sw;
+
+                if (sw == 0)
+                    n = n - 1;
+                else if (sw == 1) {
+                    n = n - 2;
+                    sw = 0;
+                }
+                continue;
+            }
+            do {
+                n  = nstk[ptr];
+                sw = sstk[ptr--] + 1;
+
+                if (sw == 2) {
+                    System.out.println(n);
+                    if (ptr < 0)
+                        return;
+                }
+            } while (sw == 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요 : ");
+        int   x = stdIn.nextInt();
+
+        recur3(x);
+    }
+}
+
