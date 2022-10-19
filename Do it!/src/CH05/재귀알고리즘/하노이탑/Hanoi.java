fd7ea763b677a8f9efa2e0a1bbfa84cdfd0d65a8
DoHee <dohee3959@gmail.com>
2022-10-19 22:03:31 +0900

    [Do it] CH05. Q6 하노이의 탑: 다른 출력

--- Do it!/src/CH05/재귀알고리즘/하노이탑/Hanoi.java
+++ Do it!/src/CH05/재귀알고리즘/하노이탑/Hanoi.java
@@ -2,7 +2,7 @@ package CH05.재귀알고리즘.하노이탑;
 
 import java.util.Scanner;
 
-/** 하노의 탑 */
+/** 하노이의 탑 */
 class Hanoi {
     // no개의 원반을 x번 기둥에서 y번 기둥으로 옮김
     static void move(int no, int x, int y) {
@@ -10,6 +10,30 @@ class Hanoi {
             move(no - 1, x, 6 - x - y);
 
         System.out.println("원반[" + no + "]을 " + x + "기둥에서 " + y + "기둥으로 옮김");
+
+        if (no > 1)
+            move(no - 1, 6 - x - y, y);
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("하노이의 탑");
+        System.out.print("원반 개수 : ");
+        int n = stdIn.nextInt();
+
+        move(n, 1, 3); // 1번 기둥의 n개의 원반을 3번 기둥으로 옮김
+    }
+}
+
+/** Q6 하노이의 탑 : 다른 출력 */
+class Hanoi2 {
+    // no개의 원반을 x번 기둥에서 y번 기둥으로 옮김
+    static void move(int no, int x, int y) {
+        if (no > 1)
+            move(no - 1, x, 6 - x - y);
+
+        System.out.println("원반[" + no + "]을 " + (x == 1 ? 'A' : x == 2 ? 'B' : 'C') + "기둥에서 " + (y == 1 ? 'A' : y == 2 ? 'B' : 'C') + "기둥으로 옮김");
 
         if (no > 1)
             move(no - 1, 6 - x - y, y);
