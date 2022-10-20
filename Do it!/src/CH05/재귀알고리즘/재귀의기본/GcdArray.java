03cc517e7b8701222fa5c5416a75aa02a470fe1d
DoHee <dohee3959@gmail.com>
2022-10-20 21:56:33 +0900

    [Do it] CH05. GCDArray

--- Do it!/src/CH05/재귀알고리즘/재귀의기본/GcdArray.java
+++ Do it!/src/CH05/재귀알고리즘/재귀의기본/GcdArray.java
@@ -1,6 +1,7 @@
 package CH05.재귀알고리즘.재귀의기본;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /** Q3. 배열 a의 모든 요소 최대 공약수 */
 public class GcdArray {
@@ -20,5 +21,47 @@ public class GcdArray {
 
     public static void main(String[] args) {
         System.out.println(gcdArray(new int[] {4, 6, 8, 10}));
+    }
+}
+
+/** 다른 형식 */
+class GCDArray {
+
+    //--- 정숫값 x, y의 최대 공약수를 비재귀적으로 구하여 반환 ---//
+    static int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return (x);
+    }
+
+    //--- 요솟수가 n인 배열 a의 모든 요소의 최대 공약수를 구합니다 ---//
+    static int gcdArray(int a[], int start, int no) {
+        if (no == 1)
+            return a[start];
+        else if (no == 2)
+            return gcd(a[start], a[start + 1]);
+        else
+            return gcd(a[start], gcdArray(a, start + 1, no - 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("몇 개 정수의 최대 공약수를 구할까요? : ");
+        int num;
+        do {
+            num = stdIn.nextInt();
+        } while (num <= 1);
+
+        int[] x = new int[num];					// 길이가 num 인 배열
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        System.out.println("최대 공약수는 " + gcdArray(x, 0, num) + "입니다.");
     }
 }
---
+++ Do it!/src/CH05/재귀알고리즘/하노이탑/HanoiNoRecur.java
@@ -0,0 +1,15 @@
+package CH05.재귀알고리즘.하노이탑;
+
+public class HanoiNoRecur {
+
+    static void move(int no, int x, int y) {
+
+        while (true) {
+            if (no > 1) {
+                no -= 1;
+
+            }
+
+        }
+    }
+}
