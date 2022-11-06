3b036ddf688e04ea7a204360041041d46aa2a4b2
DoHee <dohee3959@gmail.com>
2022-11-06 22:01:25 +0900

    [Do it] CH05 출력 다른 하노이

--- Do it!/src/CH05/재귀알고리즘/하노이탑/Hanoi.java
+++ Do it!/src/CH05/재귀알고리즘/하노이탑/Hanoi.java
@@ -49,3 +49,30 @@ class Hanoi2 {
         move(n, 1, 3); // 1번 기둥의 n개의 원반을 3번 기둥으로 옮김
     }
 }
+
+/** 배열 이용 index에 맞는 해당 값으로 변환해서 출력 */
+class HanoiEx {
+
+    static String[] name = {"A 기둥", "B 기둥", "C 기둥"};
+
+    //--- 원반을 x기둥에서 y기둥으로 이동 ---//
+    static void move(int no, int x, int y) {
+        if (no > 1)
+            move(no - 1, x, 6 - x - y);
+
+        System.out.println("원반[" + no + "]을 " + name[x - 1] + "에서 " + name[y - 1] + "으로 이동");
+
+        if (no > 1)
+            move(no - 1, 6 - x - y, y);
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("하노이의 탑");
+        System.out.print("원반의 개수 : ");
+        int n = stdIn.nextInt();
+
+        move(n, 1, 3);   // 제1기둥에 쌓인 n 개를 제3기둥으로 이동
+    }
+}
